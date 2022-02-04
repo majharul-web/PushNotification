1c6ce071425306373ce0a1bf07521aadbc439cd5
majharul <majharuljony163@gmail.com>
2022-02-04 09:38:46 +0600

    start

---
+++ App.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'black' }}>Push notification</Text>
+    </View>
+  );
+};
+
+export default App;
